5f1ff852ca0e289a62ed86053d204741c9f5b892
Yogesh Chaube <yogesh@techmojo.in>
2020-09-17 11:54:34 +0530

    Updates

---
+++ precache-manifest.25a877bf2ff8ba6c39f0f6e62fa2e375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84a68c29db7aa2e6cd4f2b962edf544",
+    "url": "/real-estate/index.html"
+  },
+  {
+    "revision": "ac929a20434e3185b464",
+    "url": "/real-estate/static/css/main.3a3405ee.chunk.css"
+  },
+  {
+    "revision": "23d595a4661fa6c0bc89",
+    "url": "/real-estate/static/js/2.4bd782ee.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/real-estate/static/js/2.4bd782ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac929a20434e3185b464",
+    "url": "/real-estate/static/js/main.383d1051.chunk.js"
+  },
+  {
+    "revision": "f845e73b8c2c285c17a1",
+    "url": "/real-estate/static/js/runtime-main.396496e6.js"
+  }
+]);
